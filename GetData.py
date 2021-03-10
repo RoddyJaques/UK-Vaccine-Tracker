e62ed2aeb9a3fb3e6cc42e19224ebc7fc708e5cb
Rod Jaques <roddy@jaques.org.uk>
2021-03-10 23:18:45 +0000

    Updated x-axis auto-scaling

--- GetData.py
+++ GetData.py
@@ -18,12 +18,12 @@ endpoint = (
             '}'
 )
 
-#Only run once data has been updated
+#Loop until data has been updated
 response = get(endpoint, timeout=10)
 last_updated = datetime.datetime.strptime(response.headers['Last-Modified'][5:16], "%d %b %Y").date()
 now = datetime.datetime.now()
 while(last_updated < now.date()):
-    print("Data not updated as of " + Now.time().strftime("%H:%M") + ", last updated: " + response.headers['Last-Modified'])
+    print("Data not updated as of " + now.time().strftime("%H:%M") + ", last updated: " + response.headers['Last-Modified'])
     time.sleep(60)
     now = datetime.datetime.now()
     response = None
@@ -50,10 +50,10 @@ by_date["7day_avg"] = by_date["vaxes"].rolling(7).mean()
 #Create numpy array of dates starting from 11/01/21 
 start_date = datetime.date(2021, 1 , 11)
 
-#Extends x-axis by 2 weeks every 2 weeks
-diff = (datetime.date.today() - datetime.date(2021,1,30)).days
-axis_add = (diff/14) + 1
-end_date = start_date + datetime.timedelta(days=(28 + 14*axis_add))
+#Extends x-axis every 2 weeks
+diff = (datetime.date.today() - datetime.date(2021,1,11)).days
+axis_add = (diff/14) + 1 
+end_date = start_date + datetime.timedelta(days=int(1.75*14*axis_add))
 end_datetime = datetime.datetime.combine(end_date, datetime.datetime.min.time()) #Keep a datetime of the end date so annoations can be plotted precisely 
 number_of_days = (end_date - start_date).days + 1
 date_list = np.asarray([(start_date + datetime.timedelta(days = day)) for day in range(number_of_days)])
